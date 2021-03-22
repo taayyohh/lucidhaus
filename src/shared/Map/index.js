d7c61d9460274956fa2866a875c5e948f02e6367
Théo Mode <theo@Theos-MBP.fios-router.home>
2021-03-22 09:20:29 -0400

    map edits

--- src/shared/Map/index.js
+++ src/shared/Map/index.js
@@ -1,4 +1,3 @@
-import {MAPBOX_PUBLIC}    from 'config'
 import mapboxgl           from 'mapbox-gl'
 import React, {useEffect} from 'react'
 import Div                from 'shared/Basic/Div'
@@ -8,7 +7,7 @@ const Map = ({lon, lat, theme}) => {
     useEffect(() => {
         if (!!lon && !!lat) {
             mapboxgl.accessToken = 'pk.eyJ1IjoiaW5jbHVzaXZlZ3VpZGUiLCJhIjoiY2ttanA1ZThnMHRkcjJ5cXU3MjJoZ2szbSJ9.0JsDAD-p_AH6RgoN1NMglA'
-            console.log('secret', MAPBOX_PUBLIC)
+            // console.log('secret', MAPBOX_PUBLIC)
             const map = new mapboxgl.Map({
                 container: 'map', // container id
                 style: 'mapbox://styles/mapbox/streets-v11', // style URL
