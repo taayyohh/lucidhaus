6758fece6e5fa09de55a18ecc4550d97880f9270
Théo Mode <theo@Theos-MBP.fios-router.home>
2021-03-22 16:40:13 -0400

    mapgl pk edit

--- src/shared/Map/index.js
+++ src/shared/Map/index.js
@@ -1,14 +1,13 @@
+import {MAPBOX_PUBLIC}    from 'config'
 import mapboxgl           from 'mapbox-gl'
 import React, {useEffect} from 'react'
 import Div                from 'shared/Basic/Div'
-import {MAPBOX_PUBLIC}    from 'config'
 
 const Map = ({lon, lat, theme}) => {
 
     useEffect(() => {
         if (!!lon && !!lat) {
             mapboxgl.accessToken = MAPBOX_PUBLIC
-            // console.log('secret', MAPBOX_PUBLIC)
             const map = new mapboxgl.Map({
                 container: 'map', // container id
                 style: 'mapbox://styles/mapbox/streets-v11', // style URL
@@ -18,16 +17,16 @@ const Map = ({lon, lat, theme}) => {
         }
     }, [lon, lat])
 
-
     return (
-        <Div id="map" theme={theme}>
-
-        </Div>
+        <Div
+            id="map"
+            theme={theme}
+        />
     )
 }
 
 Map.defaultProps = {
-    theme: {position: 'absolute', top: 0, bottom: 0, width: '100%' }
+    theme: {position: 'absolute', top: 0, bottom: 0, width: '100%'}
 }
 
 export default Map
