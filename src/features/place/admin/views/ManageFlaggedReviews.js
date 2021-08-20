25c261a0a1261e859bae209a1673dd3ccc0c83f4
Théo Mode <milesxbridges@gmail.com>
2021-08-20 13:04:29 -0400

    IGB-24 cleanup

--- src/features/place/admin/views/ManageFlaggedReviews.js
+++ src/features/place/admin/views/ManageFlaggedReviews.js
@@ -1,38 +1,20 @@
+import Review                     from 'features/user/admin/views/Review'
 import React, {useEffect}         from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import Div                        from 'shared/Basic/Div'
-import LinkSwitch                 from 'shared/Basic/LinkSwitch'
 import ContentWrapper             from 'shared/Layout/ContentWrapper'
 import AdminDashboardWrapper      from 'shared/Layout/Dashboard/admin/AdminDashboardWrapper'
 import DashboardInfo              from 'shared/Layout/Dashboard/DashboardInfo'
-import DashboardWrapper           from '../../../../shared/Layout/Dashboard/DashboardWrapper'
-import Review                     from '../../../user/admin/views/Review'
 
 const Manage = () => {
-    const {places, flaggedReviews, pendingPlaces} = useSelector(state => state.place)
-    // const {placesIndex} = useContext(searchContext)
+    const {flaggedReviews} = useSelector(state => state.place)
     const dispatch = useDispatch()
-    // const [isIndexed, setIsIndexed] = useState(false)
 
     useEffect(() => {
         dispatch({type: 'place/getFlaggedReviews'})
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-
-    useEffect(() => {
-        // if (places?.length > 0)
-        //     placesIndex.saveObjects(places)
-        //         .then(() => setIsIndexed(true))
-        //         .catch(error =>
-        //             dispatch({
-        //                 type: 'site/setNotification',
-        //                 payload: {notification: error}
-        //             })
-        //         )
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [places])
 
     return (
         <ContentWrapper>
@@ -52,16 +34,6 @@ const Manage = () => {
                             url={`/dashboard/reviews/update/${review._id}`}
                         />
                     ))}
-
-                    {/*{pendingPlaces && pendingPlaces.map((place) => (*/}
-                    {/*    <LinkSwitch*/}
-                    {/*        key={place.slug}*/}
-                    {/*        url={`/admin/places/update/${place.slug}`}*/}
-                    {/*    >*/}
-                    {/*        {place.name}*/}
-                    {/*    </LinkSwitch>*/}
-                    {/*))}*/}
-
                 </Div>
             </AdminDashboardWrapper>
         </ContentWrapper>
