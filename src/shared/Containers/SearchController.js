ffe84561841b83710cb5c1c989cbefa65a9d27dc
Théo Mode <milesxbridges@gmail.com>
2021-11-22 15:16:19 -0500

    remove extra algolia indexes

--- src/shared/Containers/SearchController.js
+++ src/shared/Containers/SearchController.js
@@ -6,21 +6,13 @@ export const searchContext = createContext(null)
 
 const SearchController = ({children}) => {
     const searchClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY)
-    const booneIndex = searchClient.initIndex('Boone')
-    const ordersIndex = searchClient.initIndex('Orders')
     const placesIndex = searchClient.initIndex('Places')
-    const productsIndex = searchClient.initIndex('Products')
-    const productCategoriesIndex = searchClient.initIndex('ProductCategories')
     const usersIndex = searchClient.initIndex('Users')
 
     return (
         <searchContext.Provider value={{
             searchClient,
-            booneIndex,
-            ordersIndex,
             placesIndex,
-            productsIndex,
-            productCategoriesIndex,
             usersIndex
         }}>
             {children}
